f5542103b3fe52852f52b4b2687beb5b1b0d382b
Kevin Carter <kevin.carter@rackspace.com>
2016-04-18 22:20:07 -0500

    Changed for lxc-host setup/build for multi-distro
    This change updates the lxc-host setup role to build the lxc cache using the
    download template based on default images found here:[0]. These images are
    upsteam builds from the greater LXC/D community.
    
    This update adds support for Ubuntu 14.04, 16.04 and RHEL/CentOS 7 container
    types and the cache will be generated from the host Operating system.
    
    [0] - https://images.linuxcontainers.org/
    
    Change-Id: Ie13be2322d28178760481c59805101d6aeef4f36
    Co-Authored-By: Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- run_tests.sh
+++ run_tests.sh
@@ -19,7 +19,11 @@ FUNCTIONAL_TEST=${FUNCTIONAL_TEST:-true}
 
 # prep the host
 if [ "$(which apt-get)" ]; then
-  apt-get install -y build-essential python2.7 python-dev git-core libssl-dev libffi-dev
+  apt-get update && apt-get install -y build-essential python2.7 python-dev git-core libffi-dev libssl-dev
+fi
+
+if [ "$(which yum)" ]; then
+  yum install -y '@Development Tools' python-devel git libffi-devel openssl-devel
 fi
 
 # get pip, if necessary
@@ -33,9 +37,9 @@ pip install tox
 
 # run through each tox env and execute the test
 for tox_env in $(awk -F= '/envlist/ {print $2}' tox.ini | sed 's/,/ /g'); do
-  if [ "${tox_env}" != "ansible-functional" ]; then
+  if [ "${tox_env}" != "functional" ]; then
     tox -e ${tox_env}
-  elif [ "${tox_env}" == "ansible-functional" ]; then
+  elif [ "${tox_env}" == "functional" ]; then
     if ${FUNCTIONAL_TEST}; then
       tox -e ${tox_env}
     fi
